b4ee648946995a65e7c8733a6928b35cdc7b6480
ylliu <liuming1908@gmail.com>
2023-01-14 15:15:47 +0800

    add generic exception handler

--- src/main/java/com/ylliu/spring/demo/rest/StudentRestController.java
+++ src/main/java/com/ylliu/spring/demo/rest/StudentRestController.java
@@ -48,4 +48,13 @@ public class StudentRestController {
         error.setTimeStamp(System.currentTimeMillis());
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception exc) {
+        StudentErrorResponse error = new StudentErrorResponse();
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
 }
